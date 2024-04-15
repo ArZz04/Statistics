b231cd3daa345ea6e83f5a8d6eb127a70533db1e
Juan <arzzdeveloper04@gmail.com>
2024-04-14 18:01:29 -0600

    update-check-values-printed

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,12 @@ public class Main {
 
         // Crear una instancia de la clase DataSet y agregar los montos
         DataSet DS = new DataSet();
-
+        int n = 0;
         // Imprimir el valor de x
         System.out.println("Valores de x: ");
         for (float value : DS.getX()) {
-            System.out.println(value);
+            n++;
+            System.out.println(n + " " + value);
         }
 
     }
